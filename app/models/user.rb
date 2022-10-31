80a8e52544b6ce6831f6d8fe910fd4e52e54fdd1
Carlos López <celb25@gmail.com>
2022-10-30 19:34:16 -0500

    📦 NEW: Initialize project for series/movies fans.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
